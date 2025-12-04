bc3325cf11b0e17cf1017484d4b33fcc06f6344c
zaysea123 <zayvos1@gmail.com>
2025-12-04 13:33:32 +0100

    edited the models.py and registerd the models so that it showed up on the admin panel

--- Holland_Home/page/admin.py
+++ Holland_Home/page/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tenant, Admin, TaskType, Room, Announcement, Task
 
-# Register your models here.
+admin.site.register(Tenant)
+admin.site.register(Admin)
+admin.site.register(TaskType)
+admin.site.register(Room)
+admin.site.register(Announcement)
+admin.site.register(Task)
